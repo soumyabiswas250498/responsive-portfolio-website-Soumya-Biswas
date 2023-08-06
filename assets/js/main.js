9b629114d0fb85fb22ca3a5d32350fde5a657577
soumyabiswas250498 <soumyabiswas250498@gmail.com>
2023-08-06 12:16:07 +0530

    commit-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,6 +73,8 @@ gsap.to('.cloud3', {
   yoyoEase: true,
 });
 
+/*=================About Page==================*/
+
 const tabSelects = [...document.querySelectorAll('#tabClick')];
 tabSelects.forEach(tabSelect => {
   tabSelect.addEventListener('click', tabScript);
@@ -109,3 +111,93 @@ function tabScript(event) {
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+VANTA.BIRDS({
+  el: '#skills',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 0.5,
+  birdSize: 1.0,
+  wingSpan: 15.0,
+  separation: 11.0,
+  quantity: 2.0,
+  backgroundColor: 0x245d6b,
+});
+
+// about me
+
+particlesJS('particles-js', {
+  particles: {
+    number: { value: 213, density: { enable: true, value_area: 800 } },
+    color: { value: '#ffffff' },
+    shape: {
+      type: 'star',
+      stroke: { width: 0, color: '#000000' },
+      polygon: { nb_sides: 5 },
+      image: { src: 'img/github.svg', width: 100, height: 100 },
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: { enable: true, mode: 'bubble' },
+      onclick: { enable: true, mode: 'repulse' },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
+      repulse: { distance: 400, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
